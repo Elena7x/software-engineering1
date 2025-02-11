f55c44a10785314b32633941595df255c6dbc9d8
Elena <elena.schwarzbach@gmx.de>
2025-02-11 11:13:42 +0100

    grobe Struktur

--- view.py
+++ view.py
@@ -1,13 +1,44 @@
 import tkinter as tk
 
-class CalendarView:
-    def __init__(self, ui, controller):
+class StudyMasterPlannerView:
+    # Hier wird Ansicht für mögliche Aktionen angezeigt
+    #
+    def __init__(self, root):
+
+    def create_menu(self):
+
+    def create_task_input_section(self, parent_frame):
+
+    def create_task_view(self, parent_frame):
+
+    def refresh_task_view(self):
+
+    def on_treeview_select(self, event):
+        # in Controller?
+
+    def show_task_input(self):
+
+    def hide_task_input(self):
 
     def add_task(self):
 
+    def update_task(self):
+
     def delete_task(self):
 
-    def update_list(self):
+    def cancel_selection(self):
+
+
+class CalendarView:
+    def __init__(self, ui, controller):
+
+    def draw_calendar(self):
+
+    def show_only_selected(self):
 
 class ListView:
     def __init__(self, ui, controller):
+
+    def draw_list(self):
+
+    def show_only_selected(self):
