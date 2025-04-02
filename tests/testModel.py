a2127d04eccfc4ecdb4fab485448d612027f9540
Elena <elena.schwarzbach@gmx.de>
2025-04-02 10:49:45 +0200

    Test von delete_entry

--- tests/testModel.py
+++ tests/testModel.py
@@ -10,6 +10,7 @@ from model import StudyMasterPlaner
 class TestStudyMasterPlaner(unittest.TestCase):
     def setUp(self):
         self.planer = StudyMasterPlaner()
+        self.planer.tasks = []
 
     def test_create_entry(self):
         self.planer.create_entry("Aufgabe 1", "2025-03-20")
@@ -24,6 +25,19 @@ class TestStudyMasterPlaner(unittest.TestCase):
         self.assertIsInstance(res, dict)
         self.assertEqual(res.get("status"), "error")
         self.assertEqual(res.get("message"), "Ungültige Eingabe: Name fehlt")
+
+    def test_delete_entry(self):
+        self.planer.create_entry(
+            "Aufgabe 1", "2025-04-05", "hoch", "Uni", "Klausur lernen", "2025-04-04"
+        )
+        self.assertEqual(len(self.planer.tasks), 1)
+
+        # Aufgabe löschen
+        result = self.planer.delete_entry("Aufgabe 1")
+
+        # Test: Aufgabe wurde entfernt
+        self.assertEqual(len(self.planer.tasks), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
